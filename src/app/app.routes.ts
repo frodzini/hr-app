c215c08dd3c088f0bd457d6f36e95460d3d99c49
Pawel Szczerbicki <pawel.szczerbicki@arcsoftware.it>
2025-08-01 13:40:05 +0200

    add routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
-import { Routes } from '@angular/router';
+import {Routes} from '@angular/router';
+import {MainComponent} from './main/main.component';
+import {DashboardComponent} from './dashboard/dashboard.component';
+import {EmployeeComponent} from './employee/employee.component';
+import {CreateEmployeeComponent} from './employee/create-employee/create-employee.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '', component: MainComponent, children: [
+      {path: '', component: DashboardComponent},
+      {path: 'employee', component: EmployeeComponent},
+      {path: 'add-employee', component: CreateEmployeeComponent},
+    ]
+  }
+];
