0a3b31a263524d57ec7201668d73dec19a9bb54a
frodzini <czanstek.tv@gmail.com>
2025-08-11 13:19:30 +0200

    Update employee component and other files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,17 @@
 import {Routes} from '@angular/router';
 import {MainComponent} from './main/main.component';
 import {DashboardComponent} from './dashboard/dashboard.component';
-import {EmployeeComponent} from './employee/employee.component';
+import {ListEmployeesComponent} from './employee/employee.component';
 import {CreateEmployeeComponent} from './employee/create-employee/create-employee.component';
+
 
 export const routes: Routes = [
   {
     path: '', component: MainComponent, children: [
       {path: '', component: DashboardComponent},
-      {path: 'employee', component: EmployeeComponent},
+      {path: 'employee', component: ListEmployeesComponent},
       {path: 'add-employee', component: CreateEmployeeComponent},
     ]
   }
 ];
+
--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,11 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Firestore, collection, getDocs } from '@angular/fire/firestore';
+import { CommonModule } from '@angular/common';
 
-@Component({
-  selector: 'app-employee',
-  imports: [],
-  templateUrl: './employee.component.html',
-  styleUrl: './employee.component.css'
+@Component({  //do html dodatek jak by 
+  selector: 'app-list-employees', 
+  standalone: true, 
+  imports: [CommonModule], // potrzebne do *ngFor
+  templateUrl: './employee.component.html', 
+  styleUrls: ['./employee.component.css'] 
 })
-export class EmployeeComponent {
+export class ListEmployeesComponent { // Komponent do wyświetlania listy pracowników
+  firestore = inject(Firestore); // To jest odwołanie do Firestore
+  employees: any[] = []; // Przechowuje listę pracowników
 
+  async ngOnInit() {
+    const ref = collection(this.firestore, 'employees'); // To jest odwołanie do kolekcji 'employees' w bazie Firestore 
+    const snapshot = await getDocs(ref);  // Pobiera dokumenty z kolekcji 'employees'
+    this.employees = snapshot.docs.map(doc => doc.data()); // Mapuje dane pracowników
+  }
 }
