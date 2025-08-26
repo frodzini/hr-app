4c84c8e10df24ba703a9a342cbef6ec6bbdba6ee
frodzini <czanstek.tv@gmail.com>
2025-08-26 13:55:13 +0200

    Dodano formularz dodawania pracowników + lista

--- src/app/employee/employee.component.ts
+++ src/app/employee/employee.component.ts
@@ -1,21 +1,40 @@
 import { Component, inject } from '@angular/core';
 import { Firestore, collection, getDocs } from '@angular/fire/firestore';
 import { CommonModule } from '@angular/common';
+import { doc, deleteDoc } from '@angular/fire/firestore';
+
 
 @Component({  //do html dodatek jak by 
   selector: 'app-list-employees', 
   standalone: true, 
-  imports: [CommonModule], // potrzebne do *ngFor
+  imports: [CommonModule], // potrzebne do ngFor 
   templateUrl: './employee.component.html', 
   styleUrls: ['./employee.component.css'] 
 })
 export class ListEmployeesComponent { // Komponent do wyświetlania listy pracowników
-  firestore = inject(Firestore); // To jest odwołanie do Firestore
+  firestore = inject(Firestore); // To jest odwołanie do Firestore 
   employees: any[] = []; // Przechowuje listę pracowników
 
   async ngOnInit() {
     const ref = collection(this.firestore, 'employees'); // To jest odwołanie do kolekcji 'employees' w bazie Firestore 
     const snapshot = await getDocs(ref);  // Pobiera dokumenty z kolekcji 'employees'
     this.employees = snapshot.docs.map(doc => doc.data()); // Mapuje dane pracowników
+    this.employees = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() })); // Dodaje id do danych pracowników 
+    console.log(this.employees); // Wyświetla listę pracowników w konsoli
   }
+  deleteEmployee(id: string) {
+  const docRef = doc(this.firestore, 'employees', id); // wskazuje na konkretny dokument po id
+  deleteDoc(docRef) // usuwa ten dokument z Firestore
+    .then(() => {
+      // po usunięciu aktualizujemy listę lokalnie, żeby zniknął z ekranu
+      this.employees = this.employees.filter(emp => emp.id !== id);
+      console.log(`Usunięto pracownika o id: ${id}`);
+    })
+    .catch(error => { //łapie błąd
+      console.error('Błąd podczas usuwania pracownika:', error);  //wyświetla błąd w konsoli
+    });
 }
+
+      }
+
+ 
